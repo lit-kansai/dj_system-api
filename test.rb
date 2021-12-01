e42d08a63657f52d3badac6ade4abaf33d86d3fd
Ritsuki Goto <develop@mizucoffee.net>
2021-12-01 10:34:18 +0900

    [add] create playlist

--- test.rb
+++ test.rb
@@ -3,41 +3,62 @@ Bundler.require
 require 'sinatra/reloader' if development?
 Dotenv.load
 require "./music/music.rb"
+require "./google.rb"
 
 enable :sessions
 
-SPOTIFY_CLIENT_ID = ENV["SPOTIFY_CLIENT_ID"]
-SPOTIFY_CLIENT_SECRET = ENV["SPOTIFY_CLIENT_SECRET"]
-REDIRECT_URL = "http://localhost:4567/api/spotify/callback"
+SPOTIFY_REDIRECT_URL = "http://localhost:4567/api/spotify/callback"
 
 before do
   request['music_api'] = Music::SpotifyApi.new(session[:spotify_token]) if session[:spotify_token]
 end
 
 get '/' do
-  "<a href=\"#{Music::SpotifyApi.get_redirect_url(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, REDIRECT_URL)}\">Spotifyログイン</a>"
+  "<a href=\"#{Music::SpotifyApi.get_oauth_url(SPOTIFY_REDIRECT_URL)}\">Spotifyログイン</a><br>"
 end
 
-get '/menu' do
+# Spotify
+
+get '/menu/spotify' do
   redirect '/' unless session[:spotify_token]
-  '<a href="/api/search?q=しわあわせ">曲検索（しわあわせ）</a><br>
-  <a href="/api/playlists">プレイリスト一覧</a>'
+  '<a href="/api/spotify/search?q=しわあわせ">曲検索（しわあわせ）</a><br>
+  <a href="/api/spotify/playlists">プレイリスト一覧</a><br>
+  <a href="/api/spotify/playlist">プレイリスト詳細</a><br>
+  <a href="/api/spotify/create_playlist?name=テスト">プレイリスト作成（テスト）</a>'
 end
 
-get '/api/search' do
+get '/api/spotify/search' do
   redirect '/' unless session[:spotify_token]
   content_type :json
   request['music_api'].search(params[:q]).to_json
 end
 
-get '/api/playlists' do
+get '/api/spotify/playlists' do
   redirect '/' unless session[:spotify_token]
   content_type :json
   request['music_api'].playlists.to_json
 end
 
+get '/api/spotify/playlist' do
+  redirect '/' unless session[:spotify_token]
+  content_type :json
+  request['music_api'].get_playlist(request['music_api'].playlists.first[:id]).to_json
+end
+
+get '/api/spotify/playlist/tracks' do
+  redirect '/' unless session[:spotify_token]
+  content_type :json
+  request['music_api'].get_playlist_tracks(request['music_api'].playlists.first[:id]).to_json
+end
+
+get '/api/spotify/create_playlist' do
+  redirect '/' unless session[:spotify_token]
+  content_type :json
+  request['music_api'].create_playlist(params[:name]).to_json
+end
+
 get '/api/spotify/callback' do
-  token = Music::SpotifyApi.get_token_by_code(params['code'], 'test', REDIRECT_URL)
+  token = Music::SpotifyApi.get_token_by_code(params['code'], 'test', SPOTIFY_REDIRECT_URL)
   session[:spotify_token] = token['access_token']
-  redirect '/menu'
+  redirect '/menu/spotify'
 end
