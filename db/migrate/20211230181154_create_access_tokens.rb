7270fc35ddf20549da0513e0f959dd757562a290
Ritsuki Goto <develop@mizucoffee.net>
2022-01-03 03:10:28 +0900

    [fix] column name

--- db/migrate/20211230181154_create_access_tokens.rb
+++ db/migrate/20211230181154_create_access_tokens.rb
@@ -2,7 +2,7 @@ class CreateAccessTokens < ActiveRecord::Migration[6.1]
   def change
     create_table :access_tokens do |t|
       t.integer :user_id
-      t.string :type
+      t.string :provider
       t.string :access_token
       t.string :refresh_token
       t.timestamps null: false
