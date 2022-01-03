9dd5d022ba8f5a020776daf1f3ad2b9b7e2dde49
{9-sho-5} <{hosho.kusu@icloud.com}>
2022-01-03 22:43:52 +0900

    Revert "user login: generate access_token & add to http header & return jwt key"
    This reverts commit d98a8465235494a688275db49a8da3cac0ea4fcd.

--- db/migrate/20220103123601_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[6.1]
-  def change
-    create_table :users do |t|
-      t.boolean :is_admin
-      t.string :google_id
-      t.timestamps null: false
-    end
-  end
-end
