d98a8465235494a688275db49a8da3cac0ea4fcd
{9-sho-5} <{hosho.kusu@icloud.com}>
2022-01-03 22:22:43 +0900

    user login: generate access_token & add to http header & return jwt key

---
+++ db/migrate/20220103123846_create_access_tokens.rb
@@ -0,0 +1,11 @@
+class CreateAccessTokens < ActiveRecord::Migration[6.1]
+  def change
+    create_table :access_tokens do |t|
+      t.integer :user_id
+      t.string :token_type
+      t.string :access_token
+      t.string :refresh_token
+      t.timestamps null: false
+    end
+  end
+end
